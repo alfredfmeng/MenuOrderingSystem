d78f3091ee4b93e3e8d589eb80aa3a534782ae91
Alfred Meng <alfredfmeng@gmail.com>
2021-12-10 12:27:32 -0600

    Created JS objects for each meal and exported meals

---
+++ menu.js
@@ -0,0 +1,22 @@
+const breakfast = {
+  1: "Eggs",
+  2: "Toast",
+  3: "Coffee",
+};
+const lunch = {
+  1: "Sandiwch",
+  2: "Chips",
+  3: "Soda",
+};
+const dinner = {
+  1: "Steak",
+  2: "Potatoes",
+  3: "Wine",
+  4: "Cake",
+};
+
+module.exports = {
+  breakfast,
+  lunch,
+  dinner,
+};
